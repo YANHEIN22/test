656dfba4fabe19c00c855fd43291834d953af9c6
YANHEIN22 <115971565+YANHEIN22@users.noreply.github.com>
2023-07-14 11:16:08 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import "./test.css"
+
+function App() {
+  return (
+    <div className='min-h-screen h-full flex items-center justify-center' data-theme="corporate">
+      <h1 className='btn btn-primary'>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
